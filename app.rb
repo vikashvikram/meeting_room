e2d689b57a2ac0210c11c8a741e1e7f69e8533c9
Vikash Vikram <vikashvikram@gmail.com>
2014-12-01 12:49:10 +0530

    adding crud for employees

--- app.rb
+++ app.rb
@@ -34,3 +34,49 @@ end
 get '/' do
   json message: "Welcome To Meeting Room App"
 end
+
+get '/employees' do
+  @employees = Employees.all
+  json @employees.to_json
+end
+
+get '/employees/:id' do
+  @employee = Employee.find(params[:id])
+  json @employee.to_json
+end
+
+post '/employees' do
+  @employee = Employee.new(params[:employee])
+  if @employee.save
+    json message: "Successfully created employee with ID: #{@employee.id}"
+  else
+    json message: "Unsuccessful employee creation", errors: @employee.errors.full_messages
+  end
+end
+
+delete '/employees/:id' do
+  @employee = Employee.find(params[:id])
+  if @employee.destroy
+    json message: "Successfully deleted"
+  else
+    json message: "Unsuccessful deletion", errors: @employee.errors.full_messages
+  end
+end
+
+put '/employees/:id' do
+  @employee = Employee.find(params[:id])
+  if @employee.update(params[:employee])
+    json message: "Successfully updated employee with ID: #{@employee.id}"
+  else
+    json message: "Unsuccessful update", errors: @employee.errors.full_messages
+  end
+end
+
+patch'/employees/:id' do
+  @employee = Employee.find(params[:id])
+  if @employee.update_attributes(params[:employee])
+    json message: "Successfully patched employee with ID: #{@employee.id}"
+  else
+    json message: "Unsuccessful patch", errors: @employee.errors.full_messages
+  end
+end
